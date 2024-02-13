c2d9285ff7ad3eec34d538fa9340700d25caf184
Pedro Ramirez <prlara2000@gmail.com>
2024-02-12 23:15:49 -0600

    add card

--- students-node-project/src/components/List.tsx
+++ students-node-project/src/components/List.tsx
@@ -12,13 +12,28 @@ interface Student {
 
 function List() {
   return (
-    <div>
+    <div className='col-12 d-flex flex-wrap justify-content-between align-content-center'>
       {students.map((student: Student) => {
         return (
-          <div key={student.id}>
-            <h1>{student.name}</h1>
-            <p>{student.gpa}</p>
-            <p>{student.email}</p>
+          <div className=' col-md-6 col-xl-4 col-xxl-3 p-2'>
+            <div className='card h-100 col-12' key={student.id}>
+              <h4 className='card-header fw-lighter'>{student.name}</h4>
+              <div className='card-body'>
+                <p className='card-text fw-bolder'>
+                  Courses: {student.courses[0]}, {student.courses[1]}, {student.courses[2]}.
+                </p>
+                <p className='card-text fw-bolder'>Phone: {student.phone}</p>
+                <p className='card-text fw-bolder'>Address: {student.address}</p>
+
+                <p className='card-text fw-bolder'>GPA: {student.gpa}</p>
+                <p>
+                  <a className='card-title fw-bolder' href={student.email}>
+                    Email: {student.email}
+                  </a>
+                </p>
+                <button className='btn btn-primary'>Update Student Profile</button>
+              </div>
+            </div>
           </div>
         );
       })}
