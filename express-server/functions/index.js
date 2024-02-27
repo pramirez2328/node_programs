b20c7f98c582b86019c287bb346d14e380c7d1c6
Pedro Ramirez <prlara2000@gmail.com>
2024-02-27 00:17:44 -0600

    add server

---
+++ express-server/functions/index.js
@@ -0,0 +1,28 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const { run } = require('./db');
+const studentsRouter = require('../routes/students');
+const cors = require('cors');
+
+// Create an Express application
+const app = express();
+
+// Configure middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+run()
+  .then(() => {
+    console.log('Connected to Database...');
+  })
+  .catch((error) => {
+    console.error('Error connecting to database:', error);
+    process.exit(1);
+  });
+
+// Define routes
+app.use('/api/students', studentsRouter);
+
+// Define your Cloud Function
+exports.app = functions.https.onRequest(app);
