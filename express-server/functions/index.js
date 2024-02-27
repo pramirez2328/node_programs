4dc896eaf8b7eb11817861433a54cce2bf98a566
Pedro Ramirez <prlara2000@gmail.com>
2024-02-26 18:48:44 -0600

    add server

---
+++ express-server/functions/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable object-curly-spacing */
+/* eslint-disable no-unused-vars */
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+// eslint-disable-next-line quotes
+const { onRequest } = require('firebase-functions/v2/https');
+// eslint-disable-next-line quotes
+const logger = require('firebase-functions/logger');
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// eslint-disable-next-line quotes
+const functions = require('firebase-functions');
+// eslint-disable-next-line quotes
+const app = require('./index'); // path to your Express app
+exports.api = functions.https.onRequest(app);
